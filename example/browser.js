d57baafb10caebd26c46a35692befaad2453f6d0
James Halliday <mail@substack.net>
2014-11-04 03:25:34 -0800

    simplifier module with slider

--- example/browser.js
+++ example/browser.js
@@ -1,17 +1,18 @@
 var loadsvg = require('load-svg');
 var createElement = require('svg-create-element');
 var rgb = require('rgb');
+var simplify = require('simplify-geometry');
+var slideways = require('slideways');
 
-loadsvg('simpler.svg', function (err, svg) {
+loadsvg('spiral.svg', function (err, svg) {
     svg.setAttribute('width', '100%');
     svg.setAttribute('height', '100%');
     var p = svg.querySelector('path');
-    var rpath = createElement('path', {
-        fill: 'transparent',
-        stroke: 'purple'
-    });
-    var points = [];
+    var pcopy = p.cloneNode(true);
+    p.parentNode.insertBefore(pcopy, p);
+    p.parentNode.removeChild(p);
     
+    var points = [];
     var len = p.getTotalLength();
     var segments = 50;
     for (var i = 0; i < segments; i++) {
@@ -19,35 +20,33 @@ loadsvg('simpler.svg', function (err, svg) {
         points.push([ pt.x, pt.y ]);
     }
     
-    var angles = [];
-    for (var i = 1; i < points.length - 1; i++) {
-        var pa = points[i-1], pb = points[i+1], pc = points[i];
-        var a = dist(pb, pc);
-        var b = dist(pa, pc);
-        var c = dist(pa, pb);
-        var angle = Math.acos((a*a + b*b - c*c) / 2 / a / b);
-        angles.push({ angle: angle, index: i });
-    }
+    var slider = slideways({ min: 0, max: 10, snap: 1, init: 5 });
+    var circles = [];
+    slider.appendTo(document.body);
     
-    angles.sort(function (a, b) {
-        return a.angle < b.angle ? -1 : 1;
+    slider.on('value', function (value) {
+        clear();
+        
+        var simplified = simplify(points, value * value);
+        simplified.forEach(function (p) {
+            plot(p[0], p[1], 1);
+        });
+        pcopy.setAttribute('d', 'M ' + simplified.join(' L '));
     });
-    
-    angles.forEach(function (a, i) {
-        var p = points[a.index];
-        plot(p[0], p[1], i / segments);
-    });
-    
-    rpath.setAttribute('d', 'M' + points.join('L'));
-    svg.appendChild(rpath);
-    
     document.body.appendChild(svg);
     
-    function plot (x, y, c) {
-        svg.appendChild(createElement('circle', {
+    function clear () {
+        circles.forEach(function (c) { svg.removeChild(c) });
+        circles = [];
+    }
+        
+    function plot (x, y) {
+        var c = createElement('circle', {
             cx: x, cy: y, r: 4, stroke: 'transparent',
-            fill: rgb('hsl(' + c*360/1.1 + ',100,50)')
-        }));
+            fill: 'magenta'
+        });
+        circles.push(c);
+        svg.appendChild(c);
     }
     function dist (a, b) {
         var x = a[0] - b[0];
