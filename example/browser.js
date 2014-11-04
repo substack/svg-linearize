af9aba7c0a3a1e399cd3373e182c0b32de224d4d
James Halliday <mail@substack.net>
2014-11-04 02:04:41 -0800

    path output

--- example/browser.js
+++ example/browser.js
@@ -5,13 +5,22 @@ loadsvg('simpler.svg', function (err, svg) {
     svg.setAttribute('width', '100%');
     svg.setAttribute('height', '100%');
     var p = svg.querySelector('path');
+    var rpath = createElement('path', {
+        fill: 'transparent',
+        stroke: 'purple'
+    });
+    var points = [];
     
     var len = p.getTotalLength();
-    var segments = 20;
+    var segments = 5;
     for (var i = 0; i < segments; i++) {
         var pt = p.getPointAtLength(i / (segments - 1) * len);
+        points.push([ pt.x, pt.y ]);
         plot(pt.x, pt.y);
     }
+    rpath.setAttribute('d', 'M' + points.join('L'));
+    svg.appendChild(rpath);
+    
     document.body.appendChild(svg);
     
     function plot (x, y) {
