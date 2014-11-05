07630e00a6199737863cf0a61d583a1fcc9de9e3
James Halliday <mail@substack.net>
2014-11-04 23:25:35 -0800

    slider on convert example

--- example/convert/browser.js
+++ example/convert/browser.js
@@ -1,7 +1,15 @@
 var linearize = require('../../');
 var loadsvg = require('load-svg');
+var slideways = require('slideways');
 
 loadsvg('spiral.svg', function (err, svg) {
+    var slider = slideways({ min: 0, max: 10, snap: 1, init: 3 });
+    var nsvg;
+    slider.on('value', function (value) {
+        if (nsvg) document.body.removeChild(nsvg);
+        nsvg = linearize(svg, { tolerance: value });
+        document.body.appendChild(nsvg);
+    });
+    slider.appendTo(document.querySelector('#controls'));
     document.body.appendChild(svg);
-    document.body.appendChild(linearize(svg));
 });
