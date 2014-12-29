8b55afa6634f633183ce9dbcabffb36822f7f6aa
James Halliday <mail@substack.net>
2014-12-29 14:20:28 -0800

    break discontinuous svg paths on z-m transitions

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var createElement = require('svg-create-element');
 var simplify = require('simplify-geometry');
 var defined = require('defined');
+var absPath = require('abs-svg-path');
+var parsePath = require('parse-svg-path');
 
 module.exports = function (svg, opts) {
     if (!opts) opts = {};
     
     var nsvg = svg.cloneNode(true);
-    var paths = nsvg.querySelectorAll('path');
+    var prepaths = nsvg.querySelectorAll('path');
+    var paths = [];
+    for (var i = 0; i < prepaths.length; i++) {
+        var pre = prepaths[i];
+        var d = pre.getAttribute('d');
+        var abs = absPath(parsePath(d)).map(unsplit).join(' ');
+        var parts = abs.match(/([^z]+(?:z\s*|$))/ig);
+        for (var j = 0; j < parts.length; j++) {
+            var el = createElement('path');
+            el.setAttribute('d', parts[j]);
+            pre.parentNode.appendChild(el);
+            paths.push(el);
+        }
+        pre.parentNode.removeChild(pre);
+    }
     
     for (var i = 0; i < paths.length; i++) {
         var p = paths[i];
@@ -17,6 +33,8 @@ module.exports = function (svg, opts) {
     }
     return nsvg;
 };
+
+function unsplit (xs) { return xs.join(' ') }
 
 function getPoints (p, segments) {
     var len = p.getTotalLength();
